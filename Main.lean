e44e3ad1e65b6fc8d2dcfb3ca11685964fcd996a
subfish-zhou <subfishzhou@gmail.com>
2026-04-10 18:47:22 +0000

    Fix Main.lean for current verso API (RenderConfig)
    KaTeX is now enabled by default in verso.

--- Main.lean
+++ Main.lean
@@ -4,11 +4,4 @@ import LeanAutoBook
 open Verso.Genre Manual
 open Verso Code External
 
-def config : Config where
-  emitHtmlSingle := .no
-  emitHtmlMulti := .immediately
-  htmlDepth := 1
-  sourceLink := some "https://github.com/subfish-zhou/lean-tacticbook"
-  issueLink := some "https://github.com/subfish-zhou/lean-tacticbook/issues"
-
-def main := manualMain (%doc LeanAutoBook) (config := { config with })
+def main := manualMain (%doc LeanAutoBook)
